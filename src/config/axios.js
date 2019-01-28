20c27d4842b9fed2a7b1e4acba9eec58f7a2fe8a
dmclavel <dmclavel@up.edu.ph>
2019-01-28 17:32:45 +0800

    Axios

--- src/config/axios.js
+++ src/config/axios.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.load();
 
 const instance = axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ? process.env.REACT_APP_MONGO_LOCAL: process.env.REACT_APP_BASE_URL
+    baseURL: process.env.NODE_ENV === 'development' ? process.env.REACT_APP_MONGO_LOCAL: process.env.REACT_APP_SERVER_BASE_URL
 });
 
 export default instance;
