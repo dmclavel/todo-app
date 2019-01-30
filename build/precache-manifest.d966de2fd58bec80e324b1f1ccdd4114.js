b1d4586a47f8d799bf4fccbcc3f4d545848801fa
dmclavel <dmclavel@up.edu.ph>
2019-01-30 18:58:38 +0800

    Realtime capabilities

---
+++ build/precache-manifest.d966de2fd58bec80e324b1f1ccdd4114.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe90d40fb87820e6e956",
+    "url": "/static/css/main.1931f773.chunk.css"
+  },
+  {
+    "revision": "fe90d40fb87820e6e956",
+    "url": "/static/js/main.fe90d40f.chunk.js"
+  },
+  {
+    "revision": "8d787f7714071a1fce34",
+    "url": "/static/js/1.8d787f77.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4adcd20854ae221e119d767f282e2c52",
+    "url": "/static/media/home-image.4adcd208.jpg"
+  },
+  {
+    "revision": "7836f876e40e04a52cea4310a6c2c58d",
+    "url": "/index.html"
+  }
+];
